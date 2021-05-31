dfe0816289d7b62c5a048240c539c5ee21e68429
LEE SEONGAHN <lisa3907@msn.com>
2021-05-31 23:26:33 +0900

    adding kucoin exchg...

---
+++ src/exchanges/sey/kucoin/private/transfer.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using CCXT.NET.Shared.Coin.Private;
+using CCXT.NET.Shared.Coin.Types;
+using CCXT.NET.Shared.Configuration;
+using System;
+
+namespace CCXT.NET.Kucoin.Private
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class BTransferItem : CCXT.NET.Shared.Coin.Private.TransferItem, ITransferItem
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "transactID")]
+        public override string transferId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "tx")]
+        public override string transactionId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string account
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public override string currency
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string transactType
+        {
+            set
+            {
+                transactionType = TransactionTypeConverter.FromString(value);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "transactStatus")]
+        public string transactStatus
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "address")]
+        public override string toAddress
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "origin_timestamp")]
+        public override long timestamp
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [JsonProperty(PropertyName = "timestamp")]
+        private DateTime timeValue
+        {
+            set
+            {
+                timestamp = CUnixTime.ConvertToUnixTimeMilli(value);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public decimal? walletBalance
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public decimal? marginBalance
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string text
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public DateTime transactTime
+        {
+            get;
+            set;
+        }
+    }
+}
